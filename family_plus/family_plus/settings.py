9e90de4edbb04fab5bb8592fd2511d4d6cb1541b
angelptli <ali95@mail.ccsf.edu>
2021-10-21 01:48:27 -0700

    Create website user database

--- family_plus/family_plus/settings.py
+++ family_plus/family_plus/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website'
+    'website',
+    'website_users',
 ]
 
 MIDDLEWARE = [
--- family_plus/website/admin.py
+++ family_plus/website/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import FamilyPlusUser
 
-# Register your models here.
+admin.site.register(FamilyPlusUser)
