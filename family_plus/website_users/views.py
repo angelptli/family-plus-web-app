0739cf35c56322121a8c4c82eae6f10a9667538e
angelptli <ali95@mail.ccsf.edu>
2021-10-21 02:20:35 -0700

    Create sign up page

--- family_plus/website_users/views.py
+++ family_plus/website_users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class UserRegisterView(generic.CreateView):
+
+    """Create user register view for registering new users with a form.
+    The success_url redirects the user to the login page after a successful
+    registration.
+
+    Inspried by: https://youtu.be/mpfHDSmqHds
+    """
+
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
