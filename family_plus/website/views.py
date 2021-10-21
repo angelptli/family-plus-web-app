2911bc26f8ac74c2d834eea1b3e3aee33a766cf2
angelptli <ali95@mail.ccsf.edu>
2021-10-20 20:54:49 -0700

    Change template name from home.html to home_page/home.html

--- family_plus/website/views.py
+++ family_plus/website/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 def home(request):
     """Render requests for home page."""
-    return render(request, 'home.html', {})
+    return render(request, 'home_page/home.html', {})
