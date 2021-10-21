20c323641a10c69204ddfb943171b6c5da707bdd
angelptli <ali95@mail.ccsf.edu>
2021-10-21 04:20:28 -0700

    Create user register view class

--- family_plus/website_users/views.py
+++ family_plus/website_users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import RegisterForm
 
 
 class UserRegisterView(generic.CreateView):
@@ -9,10 +9,13 @@ class UserRegisterView(generic.CreateView):
     """Create user register view for registering new users with a form.
     The success_url redirects the user to the login page after a successful
     registration.
-
-    Inspried by: https://youtu.be/mpfHDSmqHds
     """
 
-    form_class = UserCreationForm
+    # Credit: https://youtu.be/mpfHDSmqHds
+    # Learned how to create a class-based view and specify a redirect
+    # page for a successful user registration from this video tutorial by
+    # John Elder, owner and instructor of Codemy.com.
+    
+    form_class = RegisterForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
