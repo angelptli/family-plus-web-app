8c5c20da254d47cc9459647c74b732204e3adf5d
angelptli <ali95@mail.ccsf.edu>
2021-11-13 16:10:55 -0800

    Update fields in location app

--- family_plus/location/models.py
+++ family_plus/location/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 from location.utils import define_usa_states
 from website_users.models import FamilyProfile
-from multiselectfield import MultiSelectField
 
 class Location(models.Model):
     STATES = define_usa_states()
 
-    user    = models.ForeignKey(FamilyProfile, null=True, on_delete=models.CASCADE, related_name="related_user")
-    state   = MultiSelectField(null=True, choices=STATES)
-    city    = models.CharField(max_length=30, null=True, blank=True)
+    user      = models.ForeignKey(FamilyProfile, null=True, on_delete=models.CASCADE, related_name="related_user")
+    relation  = models.CharField(max_length=15, null=True, blank=True, choices=STATES)
+    city      = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         """Return username."""
